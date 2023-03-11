e3ef937efb115db2f236208d3075d6d7dfb448bb
Arkasha <arkadiy.gevorgyan.04@mail.ru>
2023-03-11 13:16:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$(".slider-inner").slick({
+		slidesToShow: 3,
+  		slidesToScroll: 1,
+	})
+
+	$(".browse-inner").slick({
+		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		dots: true,
+  		infinite: false,
+	})
+	
+})
+
+
